f48166db06da2e0ee8aedf5cfcd271e3d292ff0f
Xel0w <jordan.byrgiel@hotmail.fr>
2022-02-13 17:00:13 +0100

    first commit

---
+++ site/asset/js/app.js
@@ -0,0 +1,34 @@
+
+//-----------BURGER MENU--------------
+const rond= document.querySelector('.paralax');
+const rond2= document.querySelector('#paralaxmain');
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu")
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+    rond.classList.toggle('active')
+});
+
+//----------- Parallax-------------
+
+
+document.addEventListener('mousemove',parallax);
+function parallax(e){
+        
+        const speed = rond.getAttribute('data-speed')
+        const x =(window.innerWidth - e.pageX*speed)/80
+        const y =(window.innerHeight - e.pageY*speed)/80
+
+        rond.style.transform =  `translateX(${x}px) translateY(${y}px)`    
+};
+document.addEventListener('mousemove',parallax2);
+function parallax2(e){
+        
+        const speed = rond2.getAttribute('data-speed')
+        const x =(window.innerWidth - e.pageX*speed)/80
+        const y =(window.innerHeight - e.pageY*speed)/80
+
+        rond2.style.transform =  `translateX(${x}px) translateY(${y}px)`    
+};
